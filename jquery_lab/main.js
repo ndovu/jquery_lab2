f4ffe38ba941a2bcb6399bab906e34e2518c492f
Noel Bacani <mail@ndovutech.com>
2015-06-17 09:34:37 -0700

    finished the first round of attribute selector exr

--- jquery_lab/main.js
+++ jquery_lab/main.js
@@ -1,5 +1,12 @@
 $(document).on('ready', function() {
 
-  // Your Code Here
-
+  // 1. Select all the links with any "href" attribute
+  $('[href]');
+  // 2. Select all the links to "http://www.facebook.com"
+  $('a[href="http://www.facebook.com"]');
+  // 3. Highlight all the links to any ".com" URL by setting their class to "highlight".
+  $('a[href*=".com"]').attr('class', 'highlight');
+  // 4. Hide all the links NOT to "http://www.google.com"
+  $('a[href!="http://www.google.com"]').hide();
+  
 });
